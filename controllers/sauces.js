2b0689f12b9dedda5f466bdc92480b7256571959
Sylvain Yvonneau <yvonneau.sylvain@gmail.com>
2021-09-28 10:46:10 +0200

    récupère sauces

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,11 +1,23 @@
 const Sauce = require('../models/Sauces');
 
 exports.getAllSauces = (req, res, next) => {
-
+    Sauce.find()
+    .then( retour => {
+        return res.status(200).json(retour);
+    })
+    .catch( error => {
+        return res.status(400).json({error : error});
+    })
 };
 
 exports.getOneSauce = (req, res, next) => {
-
+    Sauce.findOne({_id : req.params.id})
+    .then( retour => {
+        return res.status(200).json(retour);
+    })
+    .catch( error => {
+        return res.status(404).json({error : error});
+    })
 };
 
 exports.createSauce = (req, res, next) => {
