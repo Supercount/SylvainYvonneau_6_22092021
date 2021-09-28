c9dd256697e4a2bf7c2ea7b92fe6c848d315b1a6
Sylvain Yvonneau <yvonneau.sylvain@gmail.com>
2021-09-28 10:41:46 +0200

    routes sauces

--- middleware/multer.js
+++ middleware/multer.js
@@ -19,6 +19,6 @@ const storage = multer.diskStorage({
    }
 })
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).single('image');
 
 module.exports = upload;
--- routes/sauces.js
+++ routes/sauces.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const routeur = express.Router();
 
+const sauceCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer');
+
+routeur.post('/', auth, multer, sauceCtrl.createSauce);
+routeur.put('/:id', auth, multer, sauceCtrl.modifySauce);
+routeur.delete('/:id', auth, sauceCtrl.deleteSauce);
+routeur.post('/:id/like', auth, sauceCtrl.likeSauce);
+routeur.get('/', auth, sauceCtrl.getAllSauces);
+routeur.get('/:id', auth, sauceCtrl.getOneSauce);
 
 
 module.exports = routeur;
